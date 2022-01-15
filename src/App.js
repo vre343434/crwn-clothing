5809e1a42dd2f64bb417b7233e25d3ba362abed0
vre343434 <vre343434@gmail.com>
2022-01-15 23:48:22 +0800

    add shop page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
-import { Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -16,7 +16,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route exact path='/shop/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
