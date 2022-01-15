ff20987594df90d076cd055723bba927851c4e2b
vre343434 <vre343434@gmail.com>
2022-01-15 14:05:28 +0800

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import { Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
