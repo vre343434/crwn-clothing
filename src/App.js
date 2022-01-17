4b168406a5e95c3be1aecebbd6483472a6293058
vre343434 <vre343434@gmail.com>
2022-01-17 15:57:56 +0800

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import logo from './logo.svg';
 import { Switch, Route } from 'react-router-dom';
-
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
